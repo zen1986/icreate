744744ba674b0cb5184680fb9f03af329f212db1
Zeng Qiang <zen1986@gmail.com>
2011-06-09 18:09:04 +0800

    gps dialog almost working, need testing

--- npf/src/com/npf/activity/NPFStartActivity.java
+++ npf/src/com/npf/activity/NPFStartActivity.java
@@ -2,15 +2,19 @@ package com.npf.activity;
 
 import com.npf.data.DataCache;
 import com.npf.data.MapNode;
+import com.npf.gps.GPSLocator;
 import com.npf.logic.InputManager;
 import com.npf.main.R;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -21,8 +25,12 @@ public class NPFStartActivity extends Activity {
     private String locations[];
     private DataCache dbcache;
     private InputManager im;
-    
-	/** Called when the activity is first created. */
+    private GPSLocator gps;
+    private final int WAITING = 0;
+    private final int SHOWING = 1;
+	private Location gpsLocation;
+	private ProgressDialog progressDialog;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -32,6 +40,8 @@ public class NPFStartActivity extends Activity {
         im = InputManager.getInstance(this);
 	    locations=dbcache.getLocationNames();
 	    
+	    gps = new GPSLocator(this);
+		
 	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.locationlist_item, locations);
 	    
 	    auto_origin = (AutoCompleteTextView) findViewById(R.id.autocomplete_origin);
@@ -40,10 +50,35 @@ public class NPFStartActivity extends Activity {
 	    auto_dest = (AutoCompleteTextView) findViewById(R.id.autocomplete_destination);
 	    auto_dest.setAdapter(adapter);
 	    
+
+	    
+	    
 	    final Button src_btn = (Button) findViewById(R.id.locate_src_button);
 	    src_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	showDialog(0);
+        		gps.startUpdate();
+        		
+        		progressDialog = new ProgressDialog(NPFStartActivity.this);
+        		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        		progressDialog.setMessage("Loading...");
+        		progressDialog.setCancelable(false);
+        		progressDialog.show();
+        		
+                progressDialog.show();
+                
+        		new Thread() {
+        			public void run() {
+        				while (true) {
+        					gpsLocation = gps.getBestKnowLocation();
+        					if (gpsLocation !=null) {
+        						handler.obtainMessage(0).sendToTarget();
+        						break;
+        					}
+        				}
+        			}
+        			
+        		}.start();
+        		
             }
         });
         
@@ -69,6 +104,23 @@ public class NPFStartActivity extends Activity {
         
 	}
 	
+    Handler handler = new Handler()
+    {
+        public void handleMessage(android.os.Message msg)
+        {
+            switch (msg.what)
+            {
+                case 0:
+                	progressDialog.dismiss();
+                	gps.stopUpdate();
+
+                	showDialog(SHOWING);
+                	break;
+            }
+        }
+    };
+    
+    
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK&& requestCode == 0) {
@@ -82,17 +134,29 @@ public class NPFStartActivity extends Activity {
 	}
 	
 	protected Dialog onCreateDialog (int id) {
-		final CharSequence[] items = {"Red", "Green", "Blue"};
-
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Pick nearby location");
-		builder.setItems(items, new DialogInterface.OnClickListener() {
-		    public void onClick(DialogInterface dialog, int item) {
-		    	auto_origin.setText(items[item]);
-		    }
-		});
-		AlertDialog n =  builder.create();
-		return n;
+		Dialog d;
+		switch (id) {
+		case WAITING:
+			ProgressDialog dialog = ProgressDialog.show(this, "", "Loading. Please wait...", true);
+			d = dialog;
+			break;
+		case SHOWING:
+			final CharSequence[] items = {"Red", "Green", "Blue"};
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Pick nearby location");
+			builder.setItems(items, new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog, int item) {
+			    	auto_origin.setText(items[item]);
+			    }
+			});
+			AlertDialog n =  builder.create();
+			d = n;
+			break;
+		default:
+			d = null;
+		}
+		return d;
 	}
 	
 	@Override 
